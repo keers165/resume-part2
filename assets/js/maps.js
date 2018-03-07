3cf0dcdaf6fcc19d6342f2d13362a0a0ae079a70
Keith Erskine <kerskine@gmail.com>
2018-03-07 20:02:02 +0000

    initial commit with google maps and markers added

---
+++ assets/js/maps.js
@@ -0,0 +1,36 @@
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874256 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
+
